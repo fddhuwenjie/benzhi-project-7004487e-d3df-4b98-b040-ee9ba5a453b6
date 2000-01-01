e683ce23c641a00116846e9d3ac2b16efce86545
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for 7004487e-d3df-4b98-b040-ee9ba5a453b6-bug-4

--- internal/archive/records.go
+++ internal/archive/records.go
@@ -28,7 +28,8 @@ func (s *Store) CreateArchive(batchID string, revision int, evidence []string, b
 	record := ArchiveRecord{ID: fmt.Sprintf("archive-%s-%d", batchID, revision), BatchID: batchID, FinalRevision: revision, RecordDigest: hex.EncodeToString(sum[:]), EvidenceIndex: append([]string(nil), evidence...), ArchivedBy: by, ArchivedAt: now, DigestVerified: true}
 	s.records[batchID] = record
 	if err := s.persistLocked(); err != nil {
-		return record, err
+		delete(s.records, batchID)
+		return ArchiveRecord{}, err
 	}
 	return record, nil
 }
